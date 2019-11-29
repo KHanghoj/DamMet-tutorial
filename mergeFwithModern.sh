5f6ec4e3c7982880082d2c70b6a583c8f96142b4
khanghoj <kristianhanghoej@gmail.com>
2019-11-29 15:22:00 +0100

    done

--- mergeFwithModern.sh
+++ mergeFwithModern.sh
@@ -3,6 +3,6 @@ n3=split($NF, sites, ",");
 d=0;counter=0; for (i=1;i<=length(sites);i++){
     if(exists[sites[i]]>0){
         d+=meth[sites[i]]; counter++ }else {print $4, sites[i] > "/dev/stderr"}} ;
-print $4, $14, d/counter}' MODERN_METHYLATION.data dammet_res/20.F > to_plot.txt
+print $4, $14, d/counter}' MODERN_METHYLATION.data dammet_res.20.F > to_plot.txt
 
 Rscript -e 'df=read.table("to_plot.txt");df=subset(df,V1>312500);pdf("result.pdf");plot(df[,1], df[,2],xlab="Genomic region (chr20;GRCh38)",ylab="Methylation levels",ylim=c(0,1)); lines(df[,1],df[,3],type="l", col="red");dev.off()' 
--- run.sh
+++ run.sh
@@ -16,11 +16,12 @@ M=0.75
 EXCLUDE=EXCLUDE.CpGs.txt
 # N cpgs to include per window for estimating $f$
 NCPG=50
-# PREFIX
-OUT=dammet_res/${CHROM}
+#OUT PREFIX
+OUT=dammet_res
 # Using precalculated Deamination rates as the provided bam only covers a window of 30kb bp.
 PRECALCDEAM=test.deamrates
 mkdir -p $(dirname ${OUT})
-DamMet/DamMet -b ${BAM} -r ${FASTA} -c ${CHROM} -q ${minMapQ} -Q ${minBaseQ} -P ${MAX_POS} -M ${M} -O ${OUT} -N ${NCPG} -E ${EXCLUDE} -D ${PRECALCDEAM}
+../DamMet estDEAM -b ${BAM} -r ${FASTA} -c ${CHROM} -q ${minMapQ} -Q ${minBaseQ} -P ${MAX_POS} -M ${M} -O ${OUT} -N ${NCPG} -E ${EXCLUDE} -D ${PRECALCDEAM} -skip
+../DamMet estF -b ${BAM} -r ${FASTA} -c ${CHROM} -q ${minMapQ} -Q ${minBaseQ} -P ${MAX_POS} -M ${M} -O ${OUT} -N ${NCPG} -E ${EXCLUDE} -D ${PRECALCDEAM} -skip
 
 bash mergeFwithModern.sh
