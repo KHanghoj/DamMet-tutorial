adc91abeb3b43da1063abb5ed3ee318caebcb5d7
khanghoj <kristianhanghoej@gmail.com>
2019-11-29 15:41:54 +0100

    fixed paths

--- run.sh
+++ run.sh
@@ -21,7 +21,7 @@ OUT=dammet_res
 # Using precalculated Deamination rates as the provided bam only covers a window of 30kb bp.
 PRECALCDEAM=test.deamrates
 mkdir -p $(dirname ${OUT})
-../DamMet estDEAM -b ${BAM} -r ${FASTA} -c ${CHROM} -q ${minMapQ} -Q ${minBaseQ} -P ${MAX_POS} -M ${M} -O ${OUT} -N ${NCPG} -E ${EXCLUDE} -D ${PRECALCDEAM} -skip
-../DamMet estF -b ${BAM} -r ${FASTA} -c ${CHROM} -q ${minMapQ} -Q ${minBaseQ} -P ${MAX_POS} -M ${M} -O ${OUT} -N ${NCPG} -E ${EXCLUDE} -D ${PRECALCDEAM} -skip
+DamMet/DamMet estDEAM -b ${BAM} -r ${FASTA} -c ${CHROM} -q ${minMapQ} -Q ${minBaseQ} -P ${MAX_POS} -M ${M} -O ${OUT} -N ${NCPG} -E ${EXCLUDE} -D ${PRECALCDEAM} -skip
+DamMet/DamMet estF -b ${BAM} -r ${FASTA} -c ${CHROM} -q ${minMapQ} -Q ${minBaseQ} -P ${MAX_POS} -M ${M} -O ${OUT} -N ${NCPG} -E ${EXCLUDE} -D ${PRECALCDEAM} -skip
 
 bash mergeFwithModern.sh
